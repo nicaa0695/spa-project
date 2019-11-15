b2e6e294001e6c50ef9a573de2e767316ef51105
nicaa0695 <nicaa0695@gmail.com>
2019-11-15 14:26:32 -0700

    added more clearing functions

--- spa-client/javascript/index.js
+++ spa-client/javascript/index.js
@@ -14,3 +14,26 @@ function clearNewHouseForm() {
     document.querySelector('.house-hold-input-text').value = ""
     document.querySelector('.house-members-input-text').value  = ""
 }
+
+function clearChoreDivs(){
+    choreCollection.innerHTML = ""
+}
+
+function clearNewChore() {
+    document.querySelector("#select").innerHTML = ""  
+}
+
+addHouseHoldBtn.addEventListener('click', () => {
+    addHouseHold = !addHouseHold
+    if (addHouseHold){
+        addHouseHoldBtn.textContent = "Close"
+        housePopUp.style.display = 'block'
+        housePopUp.addEventListener('submit', e => {
+            e.preventDefault()
+            HouseHold.postHouseHold(e.target)
+       })
+    } else {
+        addHouseHoldBtn.textContent = "Add a New House Hold"
+        housePopUp.style.display = 'none'
+    }
+})
