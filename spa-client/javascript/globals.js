4d069bb86751f3315a8af96d6f6b8fd5d218b131
nicaa0695 <nicaa0695@gmail.com>
2019-11-15 12:47:57 -0700

    added global variables in globas.js

---
+++ spa-client/javascript/globals.js
@@ -0,0 +1,15 @@
+const parseJSON = response => response.json()
+const addBtn = document.getElementById('new-chore-btn')
+const choreForm = document.querySelector('.container')
+const choreCollection = document.querySelector("#chore-collection")
+const familyChoresBelongTo = document.getElementById('family-chore-list')
+const housePopUp = document.getElementById('house-pop-up')
+const addHouseHoldBtn = document.getElementById('add-new-family')
+const selectHouseHoldBtn = document.getElementById('all-house-options')
+const familySelectionPopUp = document.querySelector('.family-selector')
+const selectHouseHold = document.getElementById('family-select') 
+const selectForm = document.querySelector('.family-selector')
+const select = document.querySelector("#select")
+let addChore = false 
+let addHouseHold = false
+let selectHouse = false
