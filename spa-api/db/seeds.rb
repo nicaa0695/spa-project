b16d98ea0be14d05c8fb966c7706cf4d6ceee7d8
nicaa0695 <nicaa0695@gmail.com>
2019-11-15 15:49:18 -0700

    debugging fetch

--- spa-api/db/seeds.rb
+++ spa-api/db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Chore.delete_all
-HouseHold.delete_all
-# soto = HouseHold.create(name: 'Soto', members: "Emilee Nica")
-# cook = HouseHold.create(name: 'Cook', members: "Audrea")
-# valles = HouseHold.create(name: 'Valles', members: "Arleen Randy Mary")
-# dishes = Chore.create(name: 'Dishes', status:'Complete', house_hold:valles)
-# laundry = Chore.create(name: 'Laundry', status:'Incomplete', house_hold:soto)
-# take_trash = Chore.create(name: 'Take Trash', status:'Incomplete', house_hold:cook)
+# Chore.delete_all
+# HouseHold.delete_all
+soto = HouseHold.create(name: 'Soto', members: "Emilee Nica")
+cook = HouseHold.create(name: 'Cook', members: "Audrea")
+valles = HouseHold.create(name: 'Valles', members: "Arleen Randy Mary")
+dishes = Chore.create(name: 'Dishes', status:'Complete', house_hold:valles)
+laundry = Chore.create(name: 'Laundry', status:'Incomplete', house_hold:soto)
+take_trash = Chore.create(name: 'Take Trash', status:'Incomplete', house_hold:cook)
+hillyard = HouseHold.create(name: 'Hillyard', members: "Missy John")
+mow_lawn = Chore.create(name: 'Mow Lawn', status:'Incomplete', house_hold:hillyard)
