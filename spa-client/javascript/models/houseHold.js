35878c20e0786e235f95797a6c854f72f276c70a
nicaa0695 <nicaa0695@gmail.com>
2019-11-15 14:38:31 -0700

    added fetch for households

--- spa-client/javascript/models/houseHold.js
+++ spa-client/javascript/models/houseHold.js
@@ -19,4 +19,42 @@ class HouseHold {
         let c = new Chore(chore.name, chore.status, chore.id)
         this.chores.push(c)
     }
+
+    renderChores() {
+        let familySortedChores = this.chores.sort((a,b)=>(a.name > b.name) ? 1 : -1)
+        familySortedChores.forEach(choreObj => {
+            choreObj.render()
+        })
+    }
+
+    static postHouseHold(houseHoldObj) {
+        
+        let formData = {
+            "name": houseHoldObj.name.value,
+            "members": houseHoldObj.members.value
+
+        }
+     
+        let configObj = {
+            method: "POST",
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }
+
+        return fetch(Api.HOUSE_HOLD_URL, configObj)
+            .then(response => response.json())
+            .then(houseHoldObj => {
+                let newHouseObj = new HouseHold(houseHoldObj.name, houseHoldObj.members, houseHoldObj.id)
+                return newHouseObj
+            })
+            .then(clearNewHouseForm)
+            .then(clearFamilyDD)
+            .then(clearNewChore)
+            .then(HouseHold.renderDropDownOptions)
+            .then(HouseHold.renderHouseHolds)
+
+    }
 }
