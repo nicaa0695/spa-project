dd3983fdf84328a4ff9f22cae94fec2460e6c166
nicaa0695 <nicaa0695@gmail.com>
2019-11-15 15:20:21 -0700

    added new feature for chores

--- spa-client/javascript/index.js
+++ spa-client/javascript/index.js
@@ -58,3 +58,22 @@ selectHouseHoldBtn.addEventListener('click', () => {
     }
 })
 
+addBtn.addEventListener('click', () => {
+
+    //hide and seek feature with add new chore form
+    addChore = !addChore
+    if (addChore) {
+        addBtn.textContent = 'Close'
+        choreForm.style.display = 'block'
+        choreForm.addEventListener('submit', e => {
+            e.preventDefault()
+            Chore.postChore(e.target)
+        })
+        
+        
+    } else {
+        addBtn.textContent = "Add a New Chore!"
+        choreForm.style.display = 'none'
+    }
+})
+
