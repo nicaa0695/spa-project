1d7aac049cb64f10c3167b2922f44bab3ced46d6
nicaa0695 <nicaa0695@gmail.com>
2019-11-21 15:29:10 -0700

    moving functions from chore.js to have more organization

--- spa-client/javascript/models/choreFunctions.js
+++ spa-client/javascript/models/choreFunctions.js
@@ -9,3 +9,59 @@ deleteChoreHandler() {
     })
 }
 
+completeChoreHandler() {
+    let cardIns = event.target.parentNode
+    cardIns.querySelector('.reset-chore-button').style.display = 'block'
+    event.preventDefault()
+
+    let toggleResetBtn = event.target.style.display = 'none'
+    
+
+    let statusUpdate = event.target.previousElementSibling
+    statusUpdate.innerHTML = `Completed!`
+    statusUpdate.style.color = 'green'
+
+    fetch(`${Api.CHORES_URL}/${this.id}`, {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'status': statusUpdate.textContent
+        })
+    })
+    .then(parseJSON)
+    .then(newStatus => {
+        statusUpdate
+    })
+}
+
+resetHandler() {
+    let resetStatus = event.target.previousElementSibling.previousElementSibling
+    resetStatus.innerHTML = ' Incomplete'
+    resetStatus.style.color = 'red'
+
+    let toggleCompleteBtn = event.target.previousElementSibling
+    toggleCompleteBtn.style.display = 'block'
+
+    let toggleResetBtn = event.target.style.display = 'none'
+    event.preventDefault()
+
+    fetch(`${Api.CHORES_URL}/${this.id}`, {
+        method: "PATCH",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'status': resetStatus.textContent
+        })
+    })
+    .then(parseJSON)
+    .then(newStatus => {
+        resetStatus
+    })
+    
+}
+
